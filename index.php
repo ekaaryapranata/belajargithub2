4de6e3d0af87ed3b0d12814c0c95be6a6e749578
ekaaryapranata <aryapranata46@gmail.com>
2026-02-07 20:39:38 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar git 2</title>
+</head>
+<body>
+    <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa modi mollitia vel omnis aspernatur voluptates consequatur fugit totam. Facere maiores ut numquam soluta. Dolor molestiae alias suscipit cumque quibusdam facere.
+    </p>
+</body>
+</html>
